0e25a168975197d4faa5042a88308df42bff9b78
Kévin DUBUC <kevin.dubuc@live.fr>
2012-06-06 14:54:13 +0200

    Fix du choix du layout en fonction du module

--- library/My/Layout/Controller/Plugin/Layout.php
+++ library/My/Layout/Controller/Plugin/Layout.php
@@ -2,23 +2,39 @@
 
 	class My_Layout_Controller_Plugin_Layout extends Zend_Layout_Controller_Plugin_Layout {
 	
+		private $request;
+	
 		// Modular layout
 		public function postDispatch(Zend_Controller_Request_Abstract $request) {
 		
+			$this->request = $request;
+		
 			$layout = $this->getLayout();
 			
-			if(null != ($path = file_exists(APPLICATION_PATH . '/modules/' . $request->getModuleName() . '/layouts/scripts'))) {
+			if(null != ($path = file_exists($this->getModulePath() . 'layouts' . DIRECTORY_SEPARATOR . 'scripts'))) {
 			
-				$module = $request->getModuleName();
+				$layout->setLayoutPath($this->getModulePath() . 'layouts' . DIRECTORY_SEPARATOR . 'scripts');
 			}
 			else {
 			
-				$module = 'default';
+				$layout->setLayoutPath(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'layouts' . DIRECTORY_SEPARATOR . 'scripts');
 			}
-			
-			$layout->setLayoutPath(APPLICATION_PATH . '/modules/' . $module . '/layouts/scripts');
-			
-		
+
 			parent::postDispatch($request);
+		}
+		
+		// Récupère le lien vers la racine du module
+		private function getModulePath() {
+		
+			$module = $this->request->getModuleName();
+			
+			if($module == 'default') {
+			
+				return APPLICATION_PATH;
+			}
+			else {
+			
+				return APPLICATION_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $module;
+			}
 		}
 	}

7a738485911177732993d7f71780f820de2e1ebf
Kévin DUBUC <kevin.dubuc@live.fr>
2012-06-07 12:38:03 +0200

    Indentation du Bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,18 +3,16 @@
 	class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
 	
-		public function run() {
-
-			// On appel le navigation.xml
-			$this->loadNavigation();
-			
-			// Chargement du plugin Global
-			Zend_Controller_Front::getInstance()->registerPlugin(new My_Controller_Plugin_Global);
+		public function run()
+		{
+			$this->loadNavigation();																									// On appel le navigation.xml
+			Zend_Controller_Front::getInstance()->registerPlugin(new My_Controller_Plugin_Global);		// Chargement du plugin Global
 			
 			parent::run();
 		}
 		
-		private function loadNavigation() {
+		private function loadNavigation()
+		{
 		
 			// On récupère la vue
 			$view = $this->bootstrap('view')->getResource('view');
@@ -23,7 +21,8 @@
 			$config = new Zend_Config_Xml(APPLICATION_PATH . '/configs/navigation.xml', 'nav');
 			$view->navigation( new Zend_Navigation($config->toArray()) );
 		}
-	
+		
+		/*
 		protected function _initModuleControllerDirectories()
 		{
 			$this->bootstrap('FrontController');
@@ -32,5 +31,6 @@
 			
 			$front->addControllerDirectory( APPLICATION_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . 'module' . DIRECTORY_SEPARATOR . 'controllers', 'module');
 		}
-	
+		*/
+
 	}
